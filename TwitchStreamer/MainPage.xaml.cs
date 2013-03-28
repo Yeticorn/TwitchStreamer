17960961350bfd794db06f30369acf9144b7d4d0
Zach Millford <zmillford@gmail.com>
2013-03-28 08:32:43 -0400

    UI looks like crap

---
+++ TwitchStreamer/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using TwitchStreamer.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace TwitchStreamer
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the LongListSelector control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        // Handle selection changed on LongListSelector
+        private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected item is null (no selection) do nothing
+            //if (MainLongListSelector.SelectedItem == null)
+            //    return;
+
+            // Navigate to the new page
+            //NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID, UriKind.Relative));
+
+            // Reset selected item to null (no selection)
+            //MainLongListSelector.SelectedItem = null;
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+
+    public class Channel
+    {
+        public String Title { get; set; }
+        public String User { get; set; }
+        public String Viewercount { get; set; }
+        public String Preview { get; set; }
+
+        public Channel(String title, String user, String viewercount, String preview)
+        {
+            this.Title = title;
+            this.User = user;
+            this.Viewercount = viewercount;
+            this.Preview = preview;
+        }
+    }
+
+    public class Channels : ObservableCollection<Channel>
+    {
+        public Channels()
+        {
+            Add(new Channel("Ex1 title", "Ex1 user", "Ex1 1000", "600x480"));
+            Add(new Channel("Ex2 title", "Ex2 user", "Ex2 1000", "600x480"));
+        }
+    }
+}
